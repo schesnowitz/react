9888d5ddef8ebd64586fced7a56967bc5ba6e03a
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-10 16:09:28 -0400

    forms and state

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,13 +4,7 @@ import "./some_css.css"
 
 const useState = React.useState
 
-const vehicles = [
-  { color: "Red", type: "Subaru", speed: "123", id: 123456789 },
-  { color: "Magenta", type: "Toyota", speed: "325", id: 987654321 },
-  { color: "Yellow", type: "Ford", speed: "296", id: 123123123 },
-  { color: "White", type: "Ram", speed: "114", id: 456456456 },
-  { color: "Teal", type: "Chev", speed: "696", id: 789789789 }
-]
+
 
 
 
@@ -23,11 +17,19 @@ const vehicles = [
 
 
 function OurApp() {
+  const [vehicles, setVehicles] = useState([
+    { color: "Red", type: "Subaru", speed: "123", id: 123456789 },
+    { color: "Magenta", type: "Toyota", speed: "325", id: 987654321 },
+    { color: "Yellow", type: "Ford", speed: "296", id: 123123123 },
+    { color: "White", type: "Ram", speed: "114", id: 456456456 },
+    { color: "Teal", type: "Chev", speed: "696", id: 789789789 }
+  ])
   return (
     <>
       <Header />
       <LikeArea /> 
       <Body />
+      <VehicleForm setVehicles={setVehicles} />
       <ul>
       {vehicles.map(function(vehicle) {
         return <Vehicle color={vehicle.color} type={vehicle.type} speed={vehicle.speed} key={vehicle.id} />
@@ -41,6 +43,32 @@ function OurApp() {
     </>
   )
 }
+
+function VehicleForm(props) {
+  const [color, setColor] = useState()
+  const [type, setType] = useState()
+  const [speed, setSpeed] = useState()
+  function handleSubmit(e) {
+    e.preventDefault()
+    props.setVehicles(item => item.concat({color, type, speed, id: Date.now()}))
+    // set form fields blank after submit 
+    setColor("")
+    setType("")
+    setSpeed("")
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>Add New Vehicle</legend>
+        <input value={color} onChange={e => setColor(e.target.value)} placeholder="Color" />
+        <input value={type} onChange={e => setType(e.target.value)} placeholder="Type" />
+        <input value={speed} onChange={e => setSpeed(e.target.value)} placeholder="Speed" />
+        <button>Add Vehicle</button>
+      </fieldset>
+    </form>
+  )
+}
+
 
 function LikeArea() {
   const [likeCount, setLikeCount] = useState(0)
