c5cfc8e26487549421e5cbf5a23282f41eff9b00
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-10 01:44:57 -0400

    components

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "./some_css.css"
 function OurApp() {
   return (
     <>
-      <h1 className="special">Our Amazing App Header</h1>
-      <p>The current time is {new Date().toLocaleString()}.</p>
-      <small>Copyright Footer Text</small>
+      <Header />
+      <Body />
+      <Footer />
     </>
   )
 }
 
+function Header() {
+  return <h1 className="special">App Header</h1>
+}
+
+function Body() {
+  return (
+  <p>The current time is {new Date().toLocaleString()}.</p>
+  )
+}
+
+function Footer() {
+  return (
+    <small className='teal'>Copyright Footer Text</small>
+  )
+}
 const root = ReactDOM.createRoot(document.querySelector("#app"))
 setInterval(function() {
   root.render(<OurApp />)
