72e016ed3a81e99a3132b76c317f9afa14b82481
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-10 01:03:23 -0400

    React.createElement

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+function OurApp() {
+  return React.createElement("div", null, [
+    React.createElement("h1", null, "Our Amazing App Header"),
+    React.createElement("p", null, `The current time is ${new Date().toLocaleString()}.`),
+    React.createElement("small", null, "Copyright Footer Text")
+  ])
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.querySelector("#app"))
+setInterval(function() {
+  root.render(React.createElement(OurApp))
+}, 1000)
