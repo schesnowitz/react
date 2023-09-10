b99ca0e240fd8f2b6ce2a0297c393508b06e25d1
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-10 18:10:07 -0400

    React.useEffect

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,133 +1,165 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import "./some_css.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./some_css.css";
 
-const useState = React.useState
-
-
-
-
-
-
-
-
-
-
-
-
+const useState = React.useState;
+const useEffect = React.useEffect;
 
 function OurApp() {
-  const [vehicles, setVehicles] = useState([
-    { color: "Red", type: "Subaru", speed: "123", id: 123456789 },
-    { color: "Magenta", type: "Toyota", speed: "325", id: 987654321 },
-    { color: "Yellow", type: "Ford", speed: "296", id: 123123123 },
-    { color: "White", type: "Ram", speed: "114", id: 456456456 },
-    { color: "Teal", type: "Chev", speed: "696", id: 789789789 }
-  ])
+  const [vehicles, setVehicles] = useState([]);
+
+  // only run once when page is loaded
+  // arg a is func arg b is list of things you are watching for changes
+  // useEffect(a, b)
+  useEffect(() => {
+    if (localStorage.getItem("vehicleData")) {
+      setVehicles(JSON.parse(localStorage.getItem("vehicleData")));
+    }
+  }, []);
+
+  // only run when state is changed
+  useEffect(() => {
+    localStorage.setItem("vehicleData", JSON.stringify(vehicles));
+  }, [vehicles]);
   return (
     <>
       <Header />
-      <LikeArea /> 
+      <LikeArea />
       <Body />
       <VehicleForm setVehicles={setVehicles} />
       <ul>
-      {vehicles.map(function(vehicle) {
-        return <Vehicle setVehicles={setVehicles} id={vehicle.id} color={vehicle.color} type={vehicle.type} speed={vehicle.speed} key={vehicle.id} />
-      })}
-      {/*as arrow*/}
-      <br />
-      {vehicles.map(vehicle => <Vehicle color={vehicle.color} type={vehicle.type} speed={vehicle.speed} key={vehicle.id} />
-      )}
+        {vehicles.map(function (vehicle) {
+          return (
+            <Vehicle
+              setVehicles={setVehicles}
+              id={vehicle.id}
+              color={vehicle.color}
+              type={vehicle.type}
+              speed={vehicle.speed}
+              key={vehicle.id}
+            />
+          );
+        })}
+        {/*as arrow*/}
+        <br />
+        {vehicles.map((vehicle) => (
+          <Vehicle
+            color={vehicle.color}
+            type={vehicle.type}
+            speed={vehicle.speed}
+            key={vehicle.id}
+          />
+        ))}
       </ul>
       <Footer />
     </>
-  )
+  );
 }
 
 function VehicleForm(props) {
-  const [color, setColor] = useState()
-  const [type, setType] = useState()
-  const [speed, setSpeed] = useState()
+  const [color, setColor] = useState();
+  const [type, setType] = useState();
+  const [speed, setSpeed] = useState();
   function handleSubmit(e) {
-    e.preventDefault()
-    props.setVehicles(item => item.concat({color, type, speed, id: Date.now()}))
-    // set form fields blank after submit 
-    setColor("")
-    setType("")
-    setSpeed("")
+    e.preventDefault();
+    props.setVehicles((item) =>
+      item.concat({ color, type, speed, id: Date.now() })
+    );
+    // set form fields blank after submit
+    setColor("");
+    setType("");
+    setSpeed("");
   }
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
         <legend>Add New Vehicle</legend>
-        <input value={color} onChange={e => setColor(e.target.value)} placeholder="Color" />
-        <input value={type} onChange={e => setType(e.target.value)} placeholder="Type" />
-        <input value={speed} onChange={e => setSpeed(e.target.value)} placeholder="Speed" />
+        <input
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="Color"
+        />
+        <input
+          value={type}
+          onChange={(e) => setType(e.target.value)}
+          placeholder="Type"
+        />
+        <input
+          value={speed}
+          onChange={(e) => setSpeed(e.target.value)}
+          placeholder="Speed"
+        />
         <button>Add Vehicle</button>
       </fieldset>
     </form>
-  )
+  );
 }
 
-
 function LikeArea() {
-  const [likeCount, setLikeCount] = useState(0)
+  const [likeCount, setLikeCount] = useState(0);
   function increaseLikeHandeler() {
-    setLikeCount(function(value) {
-      return value + 1
-    })
+    setLikeCount(function (value) {
+      return value + 1;
+    });
   }
   function decreaseLikeHandeler() {
-    setLikeCount(value => {
+    setLikeCount((value) => {
       if (value > 0) {
         return value - 1;
       } else {
         return 0;
       }
-    })
+    });
   }
-  return ( 
+  return (
     <>
-    <button onClick={increaseLikeHandeler}>Like More</button> 
-    <button onClick={decreaseLikeHandeler}>Like Less</button>
-    <h2>This page has been liked {likeCount} times.</h2>
+      <button onClick={increaseLikeHandeler}>Like More</button>
+      <button onClick={decreaseLikeHandeler}>Like Less</button>
+      <h2>This page has been liked {likeCount} times.</h2>
     </>
-  )
+  );
 }
 
 function Vehicle(props) {
   function handleDelete() {
-    props.setVehicles(prev => prev.filter(vehicle => vehicle.id != props.id))
+    props.setVehicles((prev) =>
+      prev.filter((vehicle) => vehicle.id != props.id)
+    );
   }
-  return (<li>A {props.color} {props.type} can go {props.speed} miles per hour.
-  <button onClick={handleDelete}>Delete</button>
-  </li>
-  )
+  return (
+    <li>
+      A {props.color} {props.type} can go {props.speed} miles per hour.
+      <button onClick={handleDelete}>Delete</button>
+    </li>
+  );
 }
 
 function Header() {
-  return <h1 className="special">App Header</h1>
+  return <h1 className="special">App Header</h1>;
 }
 
 function Body() {
-  const [getTheTiem, setTheTime] = useState(new Date().toLocaleString())
+  const [getTheTiem, setTheTime] = useState(new Date().toLocaleString());
   // setTimeout(func_here, 1000)
-  setTimeout(function() {
-    setTheTime(new Date().toLocaleString())
-  }, 1000)
-  return (
-  <p>The current time is {getTheTiem}.</p>
-  )
+  // setTimeout(function() {
+  //   setTheTime(new Date().toLocaleString())
+  // }, 1000)
+
+  useEffect((a) => {
+    const interval = setInterval(
+      () => setTheTime(new Date().toLocaleString()),
+      1000
+    );
+    // cleanup function
+    return () => clearInterval(interval);
+    // setTheTime(new Date().toLocaleString())
+  }, []);
+  return <p>The current time is {getTheTiem}.</p>;
 }
 
 function Footer() {
-  return (
-    <small className='teal'>Copyright Footer Text</small>
-  )
+  return <small className="teal">Copyright Footer Text</small>;
 }
 
-
-
-
-const root = ReactDOM.createRoot(document.querySelector("#app"))
-root.render(<OurApp />)
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<OurApp />);
