3c5470e9a78fb778573101cbb388c9559e74bd75
Stephen Chesnowitz <steve@margoscargo.com>
2023-09-10 01:08:12 -0400

    jsx

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
 function OurApp() {
-  return React.createElement("div", null, [
-    React.createElement("h1", null, "Our Amazing App Header"),
-    React.createElement("p", null, `The current time is ${new Date().toLocaleString()}.`),
-    React.createElement("small", null, "Copyright Footer Text")
-  ])
+  return (
+    <>
+      <h1 className="special">Our Amazing App Header</h1>
+      <p>The current time is {new Date().toLocaleString()}.</p>
+      <small>Copyright Footer Text</small>
+    </>
+  )
 }
 
 const root = ReactDOM.createRoot(document.querySelector("#app"))
 setInterval(function() {
-  root.render(React.createElement(OurApp))
+  root.render(<OurApp />)
 }, 1000)
